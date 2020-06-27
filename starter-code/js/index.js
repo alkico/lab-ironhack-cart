131c710c58d15214fd34cc89e344faf6c5fbf9c2
Aleah Connley <aleahkc@gmail.com>
2020-06-27 14:04:41 +0200

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,102 +1,70 @@
-var $cart = document.querySelector("#cart tbody");
-var $calc = document.getElementById("calc");
+let $cart = document.querySelector("#cart tbody");
+let $calc = document.getElementById("calc");
+let $create = document.querySelector("#create");
+let oldProduct = document.querySelector("#cart > tbody > tr:nth-child(1)");
+const cloneProductRow = oldProduct.cloneNode(true); //true for a deep copy inc. nested elements
+let productList = document.querySelectorAll("#cart > tbody > tr");
 
-let products = [
-  { name: "IronBubble-head", unitPrice: 25 },
-  { name: "Markus", unitPrice: 5 }
-];
-
-// console.log(document.getElementsByClassName('pu')[0].innerText); //access price. Index needed to reach nested element <span>
-// console.log(document.getElementsByTagName('input')); //access ALL <input> elements and creates HTML 'array'
-// console.log(document.getElementsByTagName('input')[1]); // access specific <input> element.
-// console.log(document.getElementsByTagName('input')[0].getAttribute('value')); //Type/value/min are ATTRIBUTES of <input>
-// console.log(document.getElementsByClassName('pu'));
-
-let pricesArr = document.getElementsByClassName("pu");
-//console.log(pricesArr);
-let newProductsArr = document.getElementsByClassName("new");
-//console.log(newProductsArr);
-let subtotalArr = document.getElementsByClassName("subtot");
-//console.log(subtotalArr);
-let subtotal = document.getElementsByClassName("subtot")[0].innerText;
-
-function updateSubtotal($product) {
-  let product = document.getElementsByClassName("name");
-  //console.log(product);
-  let priceUnit = parseInt(
-    document.getElementsByClassName("pu")[0].innerText.slice(1)
-  ); // This returns a string beginning with $ so use parseInt()&price.slice(2) //.slice(1); don't seem to need .slice() if use parseInt()?
-  //console.log(priceUnit);
-  let quantity = document
-    .getElementsByTagName("input")[0]
-    .getAttribute("value");
-  //console.log(quantity);
-  let subtotal = "$" + parseInt(priceUnit * quantity);
-  //console.log(subtotal);
-  let updateSubtotal = document.getElementsByClassName("subtot");
-  //console.log(updateSubtotal);
-  updateSubtotal = document.createTextNode(subtotal); // I can't get the subtotal field to update in the DOM :/
-  //console.log(updateSubtotal);
+function updateSubtotal(product) {
+  let priceUnit = product.querySelector("td.pu > span").innerHTML;
+  let quantity = product.querySelector("td.qty input").value;
+  let subtotalDiv = product.querySelector("td.subtot span");
+  subtotalDiv.innerHTML = priceUnit * quantity;
+  return priceUnit * quantity;
 }
-//ADD PRODUCT
-function addProduct(productName, priceUnit) {
-  var createProductBtn = document.getElementById("create"); //console.log(createProductBtn);
-  createProductBtn.onclick = function() {
-    let productTable = document.getElementById("cart");
-    let row = productTable.insertRow(3);
-    let productName = row.insertCell(0);
-    let productUnitePrice = row.insertCell(1);
-    productName.innerHTML = "";
-    productUnitePrice.innerHTML = "$" + "number";
-    if ((productName = "IronBubble-head")) {
-    }
-  };
 
-  //   let rows = document.getElementById("cart").rows.length;
-  //   let priceEachProduct = document.getElementById("cart").rows[1].cells[3]
-  //     .innerText;
-  //console.log(priceEachProduct);
-  // for (i=1; i < rows.length-1; i++) {
-  //  console.log(rows[i].cell[1]);
-  // }
-  //document.getElementById('cart').rows[1].cells[3].innerText = subtotal
-  //return subtotal = priceEachProduct;
-  //var x = document.getElementById("myTable").rows[0].cells.length;
+function addProduct() {
+  let createNewDiv = document.querySelector("#cart > tfoot > tr");
+  let productName = createNewDiv.querySelector(
+    "td:nth-child(1) > input[type=text]"
+  ).value;
+  let productPrice = document.querySelector(
+    "td:nth-child(2) > input[type=number]"
+  ).value;
+  let newProduct = cloneProductRow.cloneNode(true);
+  cloneProductRow.appendChild(newProduct);
 
-  //price = products[i].unitPrice;
-  //console.log(price);   //  for (let i=0; i<products.length; i++) {
-  //   products.unitPrice[i].innerText
-  // }
-  // });
+  newProduct.querySelector("td.name > span").innerHTML = productName;
+  newProduct.querySelector("td.pu > span").innerHTML = productPrice;
+  deleteOnClick(newProduct);
+  $cart.appendChild(newProduct);
 }
 
 function calculateAll() {
-  //  let sumPrice = 0;
-  updateSubtotal();
-  addProduct();
-  //  for(product in cart){
-  //  updateSubtotal(product);
-  // }
-
-  //let totalPrice = document.getElementsByTagName('h2');
-  //console.log(totalPrice);
-  //let subtotal = 0;
-  //totalPrice = subtotal;
-  //  for (let i=0; i<products.length; i++) {
-  //   products.unitPrice[i].innerText
-  // }
-
-  // for (product of products) {
-  //   product.unitPrice =
-  // }
-  // for (const item of todoListItems) {
-  //   item.innerText = item.innerText.toUpperCase();
-  // }
+  let totalPrice = 0;
+  //Note: can't use productList here because it won't include newly added products in the calc (b/c defined before their creation)
+  document.querySelectorAll("#cart > tbody > tr").forEach((product) => {
+    totalPrice += updateSubtotal(product);
+  });
+  document.querySelector("body > h2 > span").innerHTML = totalPrice.toString();
 }
 
-$calc.onclick = calculateAll;
+function deleteOnClick(product) {
+  product.querySelector(".rm > button").onclick = function () {
+    document.querySelector("#cart > tbody").removeChild(product);
+  };
+}
 
-// for (i = 0; i < products.length; i++) {
-//   let price = parseInt(document.querySelector('.pu span').innerText);
-//   let quantity = parseInt(document.querySelector('.qty label input').value); //access like an object
-//   let subtotal = products[i].unitPrice*quantity;
+productList.forEach((product) => {
+  deleteOnClick(product);
+});
+
+$calc.onclick = calculateAll;
+$create.addEventListener("click", addProduct);
+
+//PRACTICE TARGETTING ELEMENTS AND SPECIFIC ITEMS
+// console.log(document.getElementsByClassName('pu')[0].innerText); //To access price. Index needed to reach nested element <span>
+// console.log(document.getElementsByTagName('input')); //access ALL <input> elements and creates HTML 'array'
+// console.log(document.getElementsByTagName('input')[1]); // access specific <input> element.
+// console.log(document.getElementsByTagName('input')[0].getAttribute('value')); //Type/value/min are ATTRIBUTES of <input>
+
+// Get HTML arrays of elements:
+// let pricesArr = document.getElementsByClassName("pu");
+// let newProductsArr = document.getElementsByClassName("new");
+// let subtotalArr = document.getElementsByClassName("subtot");
+// let subtotal = document.getElementsByClassName("subtot")[0].innerText;
+
+//LESSONS:
+// Target specific span(element) using querySelector not getElements (unless it's an id selector in which case it's same same)
+//Use querySelector to target rows rather than Class
+//use querySelector when trying to target one element
